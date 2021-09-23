49193f790ec6cc33de6d7f80ffe0aed129f88535
mcdes <mcsors@gmail.com>
2021-09-23 13:27:50 +0200

    Added Hash_O & clean up

--- src/AleMiner.sv
+++ src/AleMiner.sv
@@ -21,11 +21,20 @@ module AleMiner
   //Registe outputs
   output VldNonce_O,
   output [5:0][31:0] Nonce_O,
+  output [7:0][31:0] Hash_O,
   
   output [31:0] HashCounter_O,
   
   // interrupt
   output Irq_O
+  
+  //ILA
+  //output [255:0] MinerHash_O,
+  //output VldHash_O,
+  //output [2:0] Cond_O,
+  //output [6:0] RdAddr_O,
+  //output RD_O,
+  //output EOM_O
   
   );
   
@@ -38,11 +47,21 @@ module AleMiner
   logic update;
   
   //number of words in the memory
-  logic [9:0] wordNum;
+  logic [ADDR_WIDTH-1:0] wordNum;
   
   // target difficulty
   logic [255:0] target;
   logic [191:0] nonce;
+  logic [255:0] hash;
+  logic [255:0] final_hash;
+  
+  // group registers
+  logic [7:0] groups; //4
+  logic [7:0] chain_num; //16 Grroups*Groups
+  logic [7:0] groups_shr;//2
+  
+  logic [7:0] from_group;
+  logic [7:0] to_group;
   
   logic [1:0] vld_hash;
   logic [1:0] rdy_hash;
@@ -62,6 +81,9 @@ module AleMiner
   logic irq;
   
   logic [31:0] hash_cntr;
+  
+  //ILA
+  //logic [2:0] conditions;
   
   // Target Difficulty
   assign target[31:0]    = Target_I[0];
@@ -84,12 +106,20 @@ module AleMiner
 
   always_ff@(posedge Clk or negedge Rst_n)
   begin : memory_update
-    if(~Rst_n)
+    if(~Rst_n) begin
       update <= 0;
-    else if(UpdateTrigger_I)
+      endOfMemory <= 0;
+    end
+    else if(UpdateTrigger_I) begin
       update <= 1'b1;
-    else if(wrAddr==(wordNum-1) && Wr_I)
+      endOfMemory <= 0;
+    end
+    else if(wrAddr==(wordNum-1) && Wr_I) begin
       update <= 0;
+      endOfMemory <= 1'b1;
+    end
+    else
+      endOfMemory <= 0;
   end
   
 ///////// 
@@ -124,20 +154,6 @@ module AleMiner
       rdAddr++;
   end
   
-////////////////// 
-// Last Message //
-//////////////////
-
-  always_ff@(posedge Clk or negedge Rst_n)
-  begin : end_of_memory
-    if(~Rst_n)
-      endOfMemory <= 0;
-    else if(wrAddr==(wordNum-1) && Wr_I)
-      endOfMemory <= 1'b1;
-    else
-      endOfMemory <= 0;
-  end
-  
 ///////////////////////// 
 // Start of Hash Cycle //
 /////////////////////////
@@ -157,17 +173,22 @@ module AleMiner
     .Update_I(strt),
     .Msg_I(msg),
     .ByteNum_I(ChunkLength_I[10:0]),
-    .FromGroup_I(GroupDirections_I[23:16]),
-    .ToGroup_I(GroupDirections_I[7:0]),
+    .FromGroup_I(from_group),
+    .ToGroup_I(to_group),
     .Nonce_I(nonce),
-    .Groups_I(Groups_I[23:16]), //4
-    .ChainNum_I(Groups_I[15:8]), //16
-    .GroupsShifter_I(Groups_I[7:0]),
+    .Groups_I(groups), //4
+    .ChainNum_I(chain_num), //16
+    .GroupsShifter_I(groups_shr),
     .Target_I(target),
     .Next_O(rd),
     .Vld_O(vld_hash[0]),
-    .Rdy_O(rdy_hash[0])
+    .Rdy_O(rdy_hash[0]),
+    .Hash_O(hash)
+    //ILA
+    //.Cond_O(conditions)
   );
+  
+  
   
   assign invld_hash = (rdy_hash==2'b01 && vld_hash==0);
   
@@ -195,9 +216,13 @@ module AleMiner
   begin : hash_counter
     if(~Rst_n)
       hash_cntr <= 0;
+    else if(UpdateTrigger_I)
+      hash_cntr <= 0;
     else if(rdy_hash==2'b01)
       hash_cntr++;
   end
+  
+  assign HashCounter_O = hash_cntr;
   
 /////////////////// 
 // Nonce Counter //
@@ -205,15 +230,26 @@ module AleMiner
 
   always_ff@(posedge Clk or negedge Rst_n)
   begin : nonce_counter
-    if(~Rst_n)
+    if(~Rst_n) begin
       nonce <= 0;
+      groups <= 0;
+      chain_num <= 0;
+      groups_shr <= 0;
+      from_group <= 0;
+      to_group <= 0;
+    end
     else if(UpdateTrigger_I) begin
       nonce[31:0]  <= Nonce_I[0];
       nonce[63:32] <= Nonce_I[1];
       nonce[95:64] <= Nonce_I[2];
       nonce[127:96]  <= Nonce_I[3];
       nonce[159:128] <= Nonce_I[4];
-      nonce[191:160] <= Nonce_I[5];    
+      nonce[191:160] <= Nonce_I[5];
+      groups <= Groups_I[23:16];
+      chain_num <= Groups_I[15:8];
+      groups_shr <= Groups_I[7:0];
+      from_group <= GroupDirections_I[23:16];
+      to_group <= GroupDirections_I[7:0];
     end
     // hashing cycle finished without valid result
     // no memory update ungoing
@@ -226,16 +262,23 @@ module AleMiner
     if(~Rst_n) begin
       vld_nonce <= 0;
       irq <= 1'b0;
+      final_hash <= {256{1'b1}};
     end
     // new mining cycle
     else if(UpdateTrigger_I) begin
       vld_nonce <= 1'b0;
       irq <= 1'b0;
+      final_hash <= {256{1'b1}};
     end
-    // new 
+    // valid hash has been found
     else begin
-      vld_nonce <= (rdy_hash==2'b01 && vld_hash==2'b01);
-      irq <= !update && vld_hash[1];
+      if (rdy_hash==2'b01 && vld_hash==2'b01) begin
+        final_hash <= hash;
+        vld_nonce <= 1'b1;
+        irq <= 1'b1;
+      end
+      else
+        vld_nonce <= 0;
     end
   end
   
@@ -248,6 +291,23 @@ module AleMiner
   assign Nonce_O[3] = nonce[127:96]; 
   assign Nonce_O[4] = nonce[159:128];
   assign Nonce_O[5] = nonce[191:160];
+  
+  assign Hash_O[0] = final_hash[31:0];
+  assign Hash_O[1] = final_hash[63:32];
+  assign Hash_O[2] = final_hash[95:64];
+  assign Hash_O[3] = final_hash[127:96]; 
+  assign Hash_O[4] = final_hash[159:128];
+  assign Hash_O[5] = final_hash[191:160];
+  assign Hash_O[6] = final_hash[223:192];
+  assign Hash_O[7] = final_hash[255:224];
+  
+  //ILA
+  //assign MinerHash_O = hash;
+  //assign VldHash_O = vld_hash[0];
+  //assign EOM_O = endOfMemory;
+  //assign RdAddr_O = rdAddr;
+  //assign RD_O = rd;
+  //assign Cond_O = conditions;
   
   
 endmodule : AleMiner
