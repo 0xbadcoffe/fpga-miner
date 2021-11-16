f1a0e089c28b944f413fc2ce2a02082dbcc05da6
mcdes <mcsors@gmail.com>
2021-11-16 12:03:20 +0000

    Added host source code for aws-fpga

---
+++ aws-fpga/src/host/constants.h
@@ -0,0 +1,10 @@
+#ifndef ALEPHIUM_CONSTANTS_H
+#define ALEPHIUM_CONSTANTS_H
+
+#define group_nums 4
+#define chain_nums 16
+#define parallel_mining_works 8
+#define mining_steps 100000
+#define BLOB_LENGTH 304
+
+#endif // ALEPHIUM_CONSTANTS_H
